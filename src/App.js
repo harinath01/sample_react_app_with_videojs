45c2e1e468fc7615e678e4e32aad736c9097b8c0
hari-testpress <hari@testpress.in>
2024-01-25 11:24:26 +0530

    Add support to play a video using HLS URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React from 'react';
 import { VideoPlayer } from "./VideoPlayer";
 import './base.css'
 
-function App() {
+const App = () => {
+  const playerRef = React.useRef(null);
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+      console.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      console.log('player will dispose');
+    });
+  };
+
   return (
     <div className="center-container">
-      <VideoPlayer width={640} height={360}/>
+      <VideoPlayer
+        width={1280}
+        height={720}
+        srcUrl="https://d384padtbeqfgy.cloudfront.net/transcoded/peBmzxeQ7Mf/video.m3u8"
+        onReady={handlePlayerReady}
+      />
     </div>
   );
 }
